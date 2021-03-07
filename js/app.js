b25da439c4eb4653d907211b4d0bb7c4ed35f4de
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-03-08 02:43:42 +0300

    add function that gets pizza products

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@ const orderContent = document.querySelector(".order-content");
 const orderItems = document.querySelector(".order-items");
 const orderTotal = document.querySelector(".order-total");
 const productsDOM = document.querySelector(".products-center");
+
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("pizzaData.json");
+      let products = await result.json();
+      console.log(products);
+
+      products = products.map((pizza) => {
+        const price = pizza.price;
+        const id = pizza.id;
+        const image = pizza.image;
+        const name = pizza.name;
+        const ingredient = pizza.ingredient;
+        return { name, price, id, image, ingredient };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
