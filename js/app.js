370ef25b1afb43de6ea79e5bb68a197e5a44eac1
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-03-08 02:40:13 +0300

    setup json file and assign values to querySelector

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-let type, size, topping, quantity, crust;
-function Pizza(type, size, quantity, crust) {
-  this.size = size;
-  this.quantity = quantity;
-  this.type = type;
-  this.crust = crust;
-  this.topping = [];
-}
-function Order(size, quantity, topping) {}
+const orderBtn = document.querySelector(".order-btn");
+const clearOrderBtn = document.querySelector(".clear-order");
+const orderDOM = document.querySelector(".order-details");
+const orderContent = document.querySelector(".order-content");
+const orderItems = document.querySelector(".order-items");
+const orderTotal = document.querySelector(".order-total");
+const productsDOM = document.querySelector(".products-center");
