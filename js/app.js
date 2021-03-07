5f1bc8c5365e6eac63f178d8f328d33ff54dc0dd
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-03-08 02:52:43 +0300

    complete functionalities for increasing and decreasing amount

--- js/app.js
+++ js/app.js
@@ -6,6 +6,24 @@ const orderItems = document.querySelector(".order-items");
 const orderTotal = document.querySelector(".order-total");
 const productsDOM = document.querySelector(".products-center");
 
+//order
+let order = [];
+//button
+let buttonsDom = [];
+
+function getSelectedSize() {
+  let selectedSizeValue = document.getElementById("size").value;
+  return selectedSizeValue;
+}
+function getSelectedCrust() {
+  let selectedCrustValue = document.getElementById("crust").value;
+  return selectedCrustValue;
+}
+function getSelectedTopping() {
+  let selectedToppingValue = document.getElementById("topping").value;
+  return selectedToppingValue;
+}
+
 class Products {
   async getProducts() {
     try {
@@ -27,7 +45,6 @@ class Products {
     }
   }
 }
-
 // display products
 class UI {
   displayProducts(products) {
@@ -51,8 +68,160 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+  getOrderButtons() {
+    let sizeArray = ["small", "medium", "large"];
+    const buttons = [...document.querySelectorAll(".order-btn")];
+    buttonsDom = buttons;
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      // let inCart = cart.find((item) => item.id === id);
+      // if (inCart) {
+      //   button.innerText = "In Cart";
+      //   button.disabled = true;
+      // }
+      button.addEventListener("click", (event) => {
+        let orderItem = { ...Storage.getProduct(id), amount: 1 };
+        console.log(orderItem);
+        //add product to the cart
+        order = [...order, orderItem];
+
+        //save cart in local storage
+        Storage.saveOrder(order);
+        //set cart values
+        this.setCartValues(order);
+        //display order item
+        this.addOrderItem(orderItem);
+      });
+    });
+  }
+  setCartValues(order) {
+    let itemTotal = 0;
+    let itemsTotal = 0;
+    order.map((item) => {
+      itemTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    orderTotal.innerText = parseFloat(itemTotal.toFixed(2));
+    //orderItems.innerText = itemsTotal;
+    console.log(itemTotal, itemsTotal);
+  }
+  addOrderItem(product) {
+    const tr = document.createElement("tr");
+    tr.classList.add(".order-item");
+    tr.innerHTML = `
+            <th scope="row">
+              <img
+                src=${product.image}
+                alt=""
+                style="width: 80px; height: 80px"
+              />
+            </th>
+            <td>${product.name}
+            
+            </td>
+            <td>
+              <select id='size' onchange="getSelectedSize()" class="form-select" aria-label="Default select example">
+                <option selected value="0">Size</option>
+                <option value="1">Small</option>
+                <option value="2">Medium</option>
+                <option value="3">Large</option>
+              </select>
+            </td>
+            <td>
+              <select id="crust" onchange="getSelectedCrust()" class="form-select" aria-label="Crust Options">
+                <option selected value="0">Crust</option>
+                <option value="1">Thin</option>
+                <option value="2">Thick</option>
+              </select>
+            </td>
+            <td>
+              <select id="topping" onchange="getSelectedTopping()" class="form-select" aria-label="Default select example">
+                <option selected value="0">Toppings</option>
+                <option value="1">Chicken</option>
+                <option value="2">Cheddar</option>
+                <option value="3">Mushroom</option>
+              </select>
+            </td>
+            <td>
+            <div>
+            <i class="fas fa-chevron-up" data-id=${product.id}></i>
+            <p class="item-amount">${product.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${product.id}></i>
+            </div>
+            </td>
+            <td>Price: ksh ${product.price}</td>
+    
+    `;
+    orderContent.appendChild(tr);
+  }
+
+  setupAPP() {
+    order = Storage.getCart();
+    this.setCartValues(order);
+    this.populateCart(order);
+  }
+  populateCart(order) {
+    order.forEach((item) => this.addOrderItem(item));
+  }
+  cartLogic() {
+    // clear cart button
+    clearOrderBtn.addEventListener("click", () => {
+      this.clearCart();
+    });
+    // cart functionality
+    orderContent.addEventListener("click", (event) => {
+      if (event.target.classList.contains("remove-item")) {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        orderContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      } else if (event.target.classList.contains("fa-chevron-up")) {
+        let addAmount = event.target;
+        let id = addAmount.dataset.id;
+        let tempItem = order.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveOrder(order);
+        this.setCartValues(order);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      } else if (event.target.classList.contains("fa-chevron-down")) {
+        let lowerAmount = event.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = order.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount - 1;
+        if (tempItem.amount >= 1) {
+          Storage.saveOrder(order);
+          this.setCartValues(order);
+          lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        } else {
+          orderContent.removeChild(
+            lowerAmount.parentElement.parentElement.parentElement
+          );
+          this.removeItem(id);
+        }
+      }
+    });
+  }
+  clearCart() {
+    let orderItems = order.map((item) => item.id);
+    orderItems.forEach((id) => this.removeItem(id));
+    console.log(orderContent.children);
+    while (orderContent.children.length > 0) {
+      orderContent.removeChild(orderContent.children[0]);
+    }
+  }
+  removeItem(id) {
+    order = order.filter((order) => order.id !== id);
+    this.setCartValues(order);
+    Storage.saveOrder(order);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+  }
+  getSingleButton(id) {
+    return buttonsDom.find((button) => button.dataset.id === id);
+  }
 }
 
+//local storage
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
@@ -70,3 +239,20 @@ class Storage {
       : [];
   }
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+  //setup app
+  ui.setupAPP();
+  //get all products
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getOrderButtons();
+      ui.cartLogic();
+    });
+});
